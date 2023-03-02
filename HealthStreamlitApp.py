3b164cc106759e609ec164701a2518a293d3a42e
Andrea <andrea.d.hobby@gmail.com>
2023-03-01 22:10:56 -0500

    Update HealthStreamlitApp.py

--- HealthStreamlitApp.py
+++ HealthStreamlitApp.py
@@ -1,41 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import streamlit as st
-
-
-# In[3]:
-
-
-import matplotlib.pyplot as plt
-
-# Generate a plot using matplotlib
-plt.plot([1, 2, 3, 4])
-plt.ylabel('some numbers')
-
-# Display the plot in the Streamlit app
-st.pyplot()
-
-
-# In[5]:
-
-
-import streamlit as st
-
-# Add a button to the app
-if st.button('Say hello'):
-    st.write('Hello!')
-
-# Add a text input field
-name = st.text_input('Enter your name')
-st.write('Hello, {}!'.format(name))
-
-
-# In[6]:
-
-
-
-
+import pandas as pd
+st.title(“A Simple Streamlit Web App”)
+name = st.text_input(“Enter your name”, ‘’)
+st.write(f”Hello {name}!”)
+x = st.slider(“Select an integer x”, 0, 10, 1)
+y = st.slider(“Select an integer y”, 0, 10, 1)
+df = pd.DataFrame({“x”: [x], “y”: [y] , “x + y”: [x + y]}, index = [“addition row”])
+st.write(df)
