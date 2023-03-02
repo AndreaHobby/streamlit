64ce82dbd224ee700b69848f2b67368c97e844d7
Andrea <andrea.d.hobby@gmail.com>
2023-03-01 22:22:08 -0500

    Update HealthStreamlitApp.py

--- HealthStreamlitApp.py
+++ HealthStreamlitApp.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
-st.title('A Simple Streamlit Web App')
+from PIL import Image
+import streamlit as st
+
+st.title('Chronic Kidney Disease Prediction')
+
+image = Image.open('./image/fnalkidney-comp_1087848275.png')
+st.image(image, caption='Sunrise by the mountains')
+
 name = st.text_input('Enter your name', '')
 st.write(f'Hello {name}!')
 x = st.slider('Select an integer x', 0, 10, 1)
@@ -8,9 +15,5 @@ y = st.slider('Select an integer y', 0, 10, 1)
 df = pd.DataFrame({'x': [x], 'y': [y] , 'x + y': [x + y]}, index = ['addition row'])
 st.write(df)
 
-from PIL import Image
-import streamlit as st
 
-image = Image.open('./image/fnalkidney-comp_1087848275.png')
-st.image(image, caption='Sunrise by the mountains')
 
