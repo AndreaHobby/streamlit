6b216b3aa942005219b7a86c35b2a5804be5a6a4
Andrea <andrea.d.hobby@gmail.com>
2023-03-01 22:28:55 -0500

    Update HealthStreamlitApp.py

--- HealthStreamlitApp.py
+++ HealthStreamlitApp.py
@@ -16,5 +16,13 @@ y = st.slider('Select an integer y', 0, 10, 1)
 df = pd.DataFrame({'x': [x], 'y': [y] , 'x + y': [x + y]}, index = ['addition row'])
 st.write(df)
 
+#create sidebar
+with st.sidebar:
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
+
+    with st.spinner("Loading..."):
+        time.sleep(5)
+    st.success("Done!")
 
 
