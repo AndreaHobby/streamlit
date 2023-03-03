81815d2f30decc1c95b3884ddac763168cda8c69
Andrea <andrea.d.hobby@gmail.com>
2023-03-03 09:32:20 -0500

    Update HealthStreamlitApp.py

--- HealthStreamlitApp.py
+++ HealthStreamlitApp.py
@@ -9,11 +9,11 @@ st.title('Chronic Kidney Disease Prediction')
 image = Image.open('./image/fnalkidney-comp_1087848275.png')
 st.image(image, caption='Sunrise by the mountains')
 
-st.write(f'Chronic kidney disease (CKD) is when the kidneys are damaged and cannot correctly filter waste and excess fluids from the blood. 
+st.write('''Chronic kidney disease (CKD) is when the kidneys are damaged and cannot correctly filter waste and excess fluids from the blood. 
 About 37 million people in the United States have Chronic Kidney Disease (CKD). Early detection and diagnosis of CKD are essential to
 preventing its progression to kidney failure. Machine learning models can assist in predicting CKD. This project is the user interface for 
 a previous project of mine that uses National Center for Health Statistics (NCHS) data. Variables such as age, gender, medical history, and laboratory
-test results will be used. By identifying patterns in the data, models can predict a patient's risk of developing CKD, allowing for early intervention and management.')
+test results will be used. By identifying patterns in the data, models can predict a patient's risk of developing CKD, allowing for early intervention and management.''')
 
 name = st.text_input('Enter your name', '')
 st.write(f'Hello {name}!')
