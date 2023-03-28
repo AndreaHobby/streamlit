e53ddb5f575b2e045aecd6151059cafdba57668b
Andrea <andrea.d.hobby@gmail.com>
2023-03-27 23:52:42 -0400

    Update HealthStreamlitApp.py

--- HealthStreamlitApp.py
+++ HealthStreamlitApp.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import streamlit as st
 import numpy as np
 import pickle
 import os 
@@ -24,15 +25,20 @@ a previous project of mine that uses National Center for Health Statistics (NCHS
 test results will be used. By identifying patterns in the data, models can predict a patient's risk of developing CKD, allowing for early intervention and management.''')
 
 
-#load the model from disk
-#import joblib
-#model = joblib.load(r"model.sav")
+#load data
+#loaded_model = pickle.load(open('Trained_DT_model.sav', 'rb'))
  
 # Define options for selectboxes
 
-
-
-careource_options = ["Doctor's Office/Clinic", "Hospital Inpatient", "Emergency Room"]
+age_options = ["<40", "40-49", "50-59", "60-69", "70+"]
+gender_options = ["Male", "Female"]
+racegrp_options = ["White", "Black", "Hispanic", "Asian", "Other"]
+education_options = ["<High School", "High School/GED", "Some College", "College Graduate"]
+marital_status_options = ["Married", "Widowed", "Divorced", "Separated", "Never Married"]
+income_options = ["<20k", "20-34k", "35-49k", "50-74k", "75-99k", "100k+"]
+ 
+  
+caresource_options = ["Doctor's Office/Clinic", "Hospital Inpatient", "Emergency Room"]
 health_insurance_options = ["Yes", "No"]
 weight_options = ["Underweight", "Normal", "Overweight", "Obese"]
 height_options = ["<5ft", "5ft-5ft5in", "5ft5in-6ft", ">6ft"]
@@ -52,95 +58,46 @@ stroke_options = ["Yes", "No"]
 cvd_options = ["Yes", "No"]
 chf_options = ["Yes", "No"]
 anemia_options = ["Yes", "No"]
-
+ 
 fam_hypertension_options = ["Yes", "No"]
 fam_diabetes_options = ["Yes", "No"]
 fam_cvd_options = ["Yes", "No"]
-
-
-	
-
-age: age_options,
-gender: gender_options,
-racegrp: racegrp_options,
-education: education_options,
-marital_status: marital_status_options,
-income: income_options,
-caresource: careource_options,
-health_insurance: health_insurance_options,
-weight: weight_options,
-height: height_options,
-sbp: sbp_options,
-dbp: dbp_options,
-hdl: hdl_options,
-ldl: ldl_options,
-total_chol: total_chol_options,
-dyslipidemia: dyslipidemia_options,
-pvd: pvd_options,
-activity: activity_options,
-poor_vision: poor_vision_options,
-smoker: smoker_options,
-hypertension: hypertension_options,
-diabetes: diabetes_options
-
-	
-
-
-def demographics():
-    st.subheader('Demographics')
-    age_options = ["<40", "40-49", "50-59", "60-69", "70+"]
-    gender_options = ["Male", "Female"]
-    racegrp_options = ["White", "Black", "Hispanic", "Asian", "Other"]
-    education_options = ["<High School", "High School/GED", "Some College", "College Graduate"]
-    marital_status_options = ["Married", "Widowed", "Divorced", "Separated", "Never Married"]
-    income_options = ["<20k", "20-34k", "35-49k", "50-74k", "75-99k", "100k+"]
-    
-    age = st.selectbox("Age", age_options)
-    gender = st.selectbox("Gender", gender_options)
-    racegrp = st.selectbox("Racegrp", racegrp_options)
-    education = st.selectbox("Education", education_options)
-    marital_status = st.selectbox("Marital Status", marital_status_options)
-    income = st.selectbox("Income", income_options)
-    
-    return age, gender, racegrp, education, marital_status, income
-
-demographics()
-   
  
-def healthcare_input():
-    st.subheader('Healthcare') 
-    caresource = st.selectbox("CareSource", caresource_options)
-    health_insurance = st.selectbox('Health Insurance', health_insurance_options)
-    weight = st.selectbox('Weight', weight_options)
-    height = st.selectbox('Height', height_options)
-    sbp = st.selectbox('SBP', sbp_options)
-    dbp = st.selectbox('DBP', dbp_options)
-    HDL = st.selectbox('HDL', hdl_options)
-    LDL = st.selectbox('LDL', ldl_options)
-    Total_Cholesterol = st.selectbox('Total Cholesterol', total_chol_options)
-    Dyslipidemia = st.selectbox('Dyslipidemia', dyslipidemia_options)
-    PVD = st.selectbox('PVD', pvd_options)
-    Activity = st.selectbox('Activity', activity_options)
-    Poor_Vision = st.selectbox('Poor Vision', poor_vision_options)
-    Smoker = st.selectbox('Smoker', smoker_options)
-    Hypertension = st.selectbox('Hypertension', hypertension_options)
-    Diabetes = st.selectbox('Diabetes', diabetes_options)
-    Stroke = st.selectbox('Stroke', stroke_options)
-    CVD = st.selectbox('CVD', cvd_options)
-    CHF = st.selectbox('CHF', chf_options)
-    Anemia = st.selectbox('Anemia', anemia_options)
-    
-    return caresource, health_insurance, weight, height, sbp, dbp, HDL, LDL, Total_Cholesterol, Dyslipidemia, PVD, Activity, Poor_Vision, Smoker, Hypertension, Diabetes, Stroke, CVD, CHF, Anemia
-healthcare_input()
+st.subheader('Demographics')   
+age = st.selectbox("Age", age_options)
+gender = st.selectbox("Gender", gender_options)
+racegrp = st.selectbox("Racegrp", racegrp_options)
+education = st.selectbox("Education", education_options)
+marital_status = st.selectbox("Marital Status", marital_status_options)
+income = st.selectbox("Income", income_options)
+ 
+st.subheader('Healthcare') 
+caresource = st.selectbox("CareSource", caresource_options)
+health_insurance = st.selectbox('Health Insurance', health_insurance_options)
+weight = st.selectbox('Weight', weight_options)
+height = st.selectbox('Height', height_options)
+sbp = st.selectbox('SBP', (0, 20, 40, 60, 80, 100))
+dbp = st.selectbox('DBP', (0, 20, 40, 60, 80, 100))
+HDL = st.selectbox('HDL', [0, 1, 2, 3])
+LDL = st.selectbox('LDL', [0, 1, 2, 3])
+Total_Cholesterol = st.selectbox('Total Cholesterol', [0, 1, 2, 3])
+Dyslipidemia = st.selectbox('Dyslipidemia', [0, 1])
+PVD = st.selectbox('PVD', [0, 1])
+Activity = st.selectbox('Activity', [0, 1, 2])
+Poor_Vision = st.selectbox('Poor Vision', [0, 1])
+Smoker = st.selectbox('Smoker', [0, 1])
+Hypertension = st.selectbox('Hypertension', [0, 1])
+Diabetes = st.selectbox('Diabetes', [0, 1])
+Stroke = st.selectbox('Stroke', stroke_options)
+CVD = st.selectbox('CVD', [0, 1])
+CHF = st.selectbox('CHF', [0, 1])
+Anemia = st.selectbox('Anemia', [0, 1])
 
-def family_history():
-    st.subheader('Family History')
-    fam_hypertension = st.selectbox('Fam Hypertension', fam_hypertension_options)
-    fam_diabetes = st.selectbox('Fam Diabetes', fam_diabetes_options)
-    fam_cvd = st.selectbox('Fam CVD', fam_cvd_options)
-    
-    return fam_hypertension, fam_diabetes, fam_cvd
-family_history()
+st.subheader('Family History') 
+Fam_Hypertension = st.selectbox('Fam Hypertension', [0, 1])
+Fam_Diabetes = st.selectbox('Fam Diabetes', [0, 1])
+Fam_CVD = st.selectbox('Fam CVD', [0, 1])
+ 
 
    # Create button to run model
 if st.button("CKD Risk Result"):
@@ -159,7 +116,6 @@ with st.sidebar:
             
             Please note that this tool is a work in progress. Contact me [here](mailto:ahobby@healthdatasciencenewsletter) or consider checking out [GitHub](https://github.com/AndreaHobby/CKD-Prediction/blob/main/Predicting%20Chronic%20Kidney%20Disease.ipynb) repository with any suggestions or questions.     
         """)
-
 
 
 
